3bb47bb88c449b9f2e65b4430bbb90297d683417
Omar Ajerray <oajerray@pushtechnology.com>
2018-09-24 18:08:54 +0100

    removed unnecessary files.

--- basic-chat-demo/src/create-channel.ts
+++
@@ -1,6 +0,0 @@
-import * as Diffusion from 'diffusion';
-
-Diffusion.connect({ host: window.location.host, principal: 'control', credentials: 'password' }).then((session) => {
-    this.chatSession = session;
-    return session.topics.add('chat/channel', Diffusion.topics.TopicType.JSON).then((res) => console.log('Chat channel created'));
-});
